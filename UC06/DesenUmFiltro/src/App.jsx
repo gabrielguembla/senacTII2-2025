edcd705c55659cd66675f1185cc5c3afd743e847
Gabriel Guembla <gabrieln.miguel@hotmail.com>
2026-08-27 18:44:43 -0300

    I created the folder UC06

---
+++ UC06/DesenUmFiltro/src/App.jsx
@@ -0,0 +1,129 @@
+import { Fragment, useState } from 'react'
+
+import './App.css'
+
+// const searchBar = window.document.querySelector("#searchBar");
+// const btn = window.document.querySelector("#btn");
+// const result = window.document.querySelector("#result");
+
+function App() {
+
+  const animes = [
+    {
+      id: 1,
+      animeTitle: "Naruto",
+      releaseDate: "2002",
+      releaseStatus: "Finished"
+    },
+    {
+      id: 2,
+      animeTitle: "One Piece",
+      releaseDate: "1999",
+      releaseStatus: "Ongoing"
+    },
+    {
+      id: 3,
+      animeTitle: "Dragon Ball Z",
+      releaseDate: "1989",
+      releaseStatus: "Finished"
+    },
+    {
+      id: 4,
+      animeTitle: "Attack on Titan",
+      releaseDate: "2013",
+      releaseStatus: "Finished"
+    },
+    {
+      id: 5,
+      animeTitle: "Demon Slayer",
+      releaseDate: "2019",
+      releaseStatus: "Ongoing"
+    },
+    {
+      id: 6,
+      animeTitle: "My Hero Academia",
+      releaseDate: "2016",
+      releaseStatus: "Finished"
+    },
+    {
+      id: 7,
+      animeTitle: "Jujutsu Kaisen",
+      releaseDate: "2020",
+      releaseStatus: "Ongoing"
+    },
+    {
+      id: 8,
+      animeTitle: "Death Note",
+      releaseDate: "2006",
+      releaseStatus: "Finished"
+    },
+    {
+      id: 9,
+      animeTitle: "Fullmetal Alchemist: Brotherhood",
+      releaseDate: "2009",
+      releaseStatus: "Finished"
+    },
+    {
+      id: 10,
+      animeTitle: "Hunter x Hunter",
+      releaseDate: "2011",
+      releaseStatus: "Hiatus"
+    }
+  ];
+
+  console.log(animes);
+
+  const [searchQuery, setSearchQuery] = useState('');
+
+
+
+  const filteredAnimes = animes.filter(anime => {
+    const idMatch = anime.id !== "" && anime.id === Number(searchQuery);
+    const animeTitleMatch = anime.animeTitle.toLocaleLowerCase().includes(searchQuery.toLowerCase());
+    const releaseDatematch = anime.releaseDate.toLocaleLowerCase().includes(searchQuery.toLowerCase());
+    const releaseStatusMatch = anime.releaseStatus.toLocaleLowerCase().includes(searchQuery.toLocaleLowerCase())
+
+
+    return idMatch || animeTitleMatch || releaseDatematch || releaseStatusMatch
+
+  });
+
+
+
+
+  return (
+    <>
+      <form action="">
+        <label htmlFor="searchBar"></label>
+        <input type="text" name="searchBar" id="searchBar" placeholder='Search by Id' onChange={(e) => setSearchQuery(e.target.value)}/>
+
+        {/* <button type="button" id='button' >Pequisar</button> */}
+
+      </form>
+
+      <section>
+        <h2>Animes Informations</h2>
+
+        <div id="result">
+          <ul>
+            {filteredAnimes.length > 0 ? (filteredAnimes.map(user => {
+
+              return <li key={user.id}>
+                <p>Title: {user.animeTitle}</p>
+                <p>Release Date: {user.releaseDate}</p>
+                <p>Release Status: {user.releaseStatus}</p>
+              </li>
+
+            })) : (
+              <p>No results found...</p>
+            )}
+
+          </ul>
+        </div>
+      </section>
+    </>
+
+  )
+}
+
+export default App
